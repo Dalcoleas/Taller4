d4fbe7ba842d924ec4c9a195b0f52bf869574ad7
Dalcoleas <00048015@uca.edu.sv>
2019-05-21 21:03:06 -0600

    Adapter

---
+++ app/src/main/java/com/example/taller4/adapters/BookListAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.taller4.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.taller4.database.entities.Book
+
+/*class BookListAdapter internal constructor(context: Context): RecyclerView.Adapter<BookListAdapter.BookViewHolder>(){
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+    private var books = emptyList<Book>() // Copia de palabras que están en caché
+
+    inner class BookViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
+
+    override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
+
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookListAdapter.BookViewHolder {
+
+    }
+
+    override fun getItemCount() = books.size
+    }
+
+
+    internal fun setBooks(books: List<Book>){
+        this.books = books
+        notifyDataSetChanged()
+    }
+
+
+}*/
