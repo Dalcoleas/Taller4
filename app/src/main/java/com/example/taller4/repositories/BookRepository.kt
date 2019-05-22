9eaa632f5c043e128945244ee1bf74f87d6f64fe
Vixtor61 <00198117@uca.edu.sv>
2019-05-21 22:13:15 -0600

    repository

---
+++ app/src/main/java/com/example/taller4/repositories/BookRepository.kt
@@ -0,0 +1,18 @@
+package com.example.taller4.repositories
+
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+import com.example.taller4.database.entities.Book
+/*
+class BookRepository(private val bookDao : BookDao){
+
+    /*
+    val allBooks : LiveData<List<Book>> = BookDao.getAll()
+
+    @WorkerThread
+    suspend fun insert(book: BookDao){
+        BookDao.insert(book)
+
+    }
+
+}*/
