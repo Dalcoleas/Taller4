52153c61c31ee5bd3ecf03a5da8bb5ae0d84feb7
Tania Orellana <00123717@uca.edu.sv>
2019-05-23 16:57:24 -0600

    Agregar funciones para resto de entidades.

--- app/src/main/java/com/example/taller4/viewmodels/BookViewModel.kt
+++ app/src/main/java/com/example/taller4/viewmodels/BookViewModel.kt
@@ -1,44 +1,111 @@
-//package com.example.taller4.viewmodels
-//
-//import android.app.Application
-//import androidx.lifecycle.AndroidViewModel
-//import androidx.lifecycle.LiveData
-//import androidx.lifecycle.viewModelScope
-//import kotlinx.coroutines.Dispatchers
-//import kotlinx.coroutines.launch
-//
-//class BookViewModel(
-//    app: Application
-//) : AndroidViewModel(app) {
-//
-//    private val repository: BookRepository
-//
-//    init {
-//        val bookDao = BookRoomDatabase.getInstance(app).BookDao()
-//
-//        repository = BookRepository(bookDao)
-//    }
-//
-//    fun getAllBooks(): LiveData<List<Book>> = repository.getAll()
-//
-// /** Buscar libro según título **/
-//    fun searchBookByTitle(title: String): LiveData<List<Book>> = repository.searchByTitle(title)
-//
-// /** Buscar libro según Author **/
-//    fun searchBookByAuthor(author: String): LiveData<List<Book>> = repository.searchByAuthor(author)
-//
-// /** Buscar libro según Editorial**/
-//    fun searchBookByPublisher(publisher: String): LiveData<List<Book>> = repository.searchByPublisher(publisher)
-//
-// /** Buscar libro según Tag**/
-//    fun searchBookByTag(publisher: String): LiveData<List<Book>> = repository.searchByTag(tag)
-//
-// /** Buscar libro según ISBN**/
-//    fun searchBookByPublisher(isbn: String): LiveData<List<Book>> = repository.searchByIsbn(isbn)
-//
-//    fun insert(book: Book) =
-//        viewModelScope.launch(Dispatchers.IO)
-//        {
-//            repository.insert(book)
-//        }
-//}
+package com.example.taller4.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.taller4.database.entities.Author
+import com.example.taller4.database.entities.Book
+import com.example.taller4.database.entities.Publisher
+import com.example.taller4.database.entities.Tag
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class BookViewModel(
+    app: Application
+) : AndroidViewModel(app) {
+
+    private val repository: BookRepository
+
+    init {
+        val bookDao = BookRoomDatabase.getInstance(app).BookDao()
+
+        repository = BookRepository(bookDao)
+    }
+
+    /** Bloque de Book **/
+    fun insert(book: Book) =
+        viewModelScope.launch(Dispatchers.IO)
+        {
+            repository.insert(book)
+        }
+
+    /** Bloque de funciones de búsqueda de libros**/
+    /** Obtener todos los libros **/
+    fun getAllBooks(): LiveData<List<Book>> =
+        repository.getAllBooks()
+
+    /** Buscar libro según id **/
+    fun getBook(id: Int): LiveData<Book> =
+        repository.getBook(id)
+
+    /** Buscar libro según título **/
+    fun searchBookByTitle(title: String): LiveData<List<Book>> =
+        repository.searchByTitle(title)
+
+    /** Buscar libro según Author **/
+    fun searchBookByAuthor(author: String): LiveData<List<Book>> =
+        repository.searchByAuthor(author)
+
+    /** Buscar libro según Editorial **/
+    fun searchBookByPublisher(publisher: String): LiveData<List<Book>> =
+        repository.searchByPublisher(publisher)
+
+    /** Buscar libro según Tag **/
+    fun searchBookByTag(tag: String): LiveData<List<Book>> =
+        repository.searchByTag(tag)
+
+    /** Buscar libro según ISBN **/
+    fun searchBookByIsbn(isbn: String): LiveData<List<Book>> =
+        repository.searchByIsbn(isbn)
+    /** Fin de bloque de funciones de búsqueda **/
+    /** Fin de bloque de Book **/
+
+
+    /** Bloque de Author **/
+    fun insert(author: Author) =
+        viewModelScope.launch(Dispatchers.IO)
+        {
+            repository.insert(author)
+        }
+
+    /** Obtener todos los autores **/
+    fun getAllAuthors(): LiveData<List<Author>> = repository.getAllAuthors()
+
+    /** Buscar autor según id **/
+    fun getAuthor(id: Int): LiveData<Author> =
+        repository.getAuthor(id)
+    /** Fin de bloque de Author **/
+
+
+    /** Bloque de Tag**/
+    fun insert(tag: Tag) =
+        viewModelScope.launch(Dispatchers.IO)
+        {
+            repository.insert(tag)
+        }
+
+    /** Obtener todas las tags **/
+    fun getAllTags(): LiveData<List<Tag>> = repository.getAllTags()
+
+    /** Buscar tag según id **/
+    fun getTag(id: Int): LiveData<Tag> =
+        repository.getTag(id)
+    /** Fin de bloque de Tag **/
+
+
+    /** Bloque de Publisher **/
+    fun insert(publisher: Publisher) =
+        viewModelScope.launch(Dispatchers.IO)
+        {
+            repository.insert(publisher)
+        }
+
+    /** Obtener todas las casas editoras **/
+    fun getAllPublishers(): LiveData<List<Publisher>> = repository.getAllPulishers()
+
+    /** Buscar tag según id **/
+    fun getPublisher(id: Int): LiveData<Publisher> =
+        repository.getPublisher(id)
+    /** Fin de bloque de Publisher **/
+}
